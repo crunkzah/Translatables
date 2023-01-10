b22f68765604c3c35dcb97a03815a06b7a27e7b5
Marat Nugmanov <nmg1033@gmail.com>
2023-01-10 03:43:35 +0300

    TextArea attribute

--- Assets/TranslatableText/TranslatableText.cs
+++ Assets/TranslatableText/TranslatableText.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TranslatableText_ScriptableObject", menuName = "Translatable/New Translatable Text")]
 public class TranslatableText : ScriptableObject
 {
-    public string eng_str;
-    public string spanish_str;
-    public string russian_str;
-    public string polish_str;
-
-
-
+    [TextArea(1, 5)]public string eng_str;
+    [TextArea(1, 5)]public string spanish_str;
+    [TextArea(1, 5)]public string russian_str;
+    [TextArea(1, 5)]public string polish_str;
 }
